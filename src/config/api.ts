eb122cb660c7e85fd6fd55de4731928ad4b09aa7
Kinda Hall <1142704468@qq.com>
2024-12-10 00:14:03 +0800

    feat: 🎸 修改生产环境域名

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 export const UNIAPI_PREFIX = 'https://api.uniapi.top';
 console.log(process.env.NODE_ENV, process.env.TZ)
-export const API_PREFIX = process.env.NODE_ENV == 'production' ? 'https://api.receiver.qingyun.com' : 'http://localhost:8000'
-export const LOGIN_WS_URL = process.env.NODE_ENV == 'production' ? 'wss://api.receiver.qingyun.com/login' : 'ws://localhost:8000/login'
+export const API_PREFIX = process.env.NODE_ENV == 'production' ? 'https://api.receiver.qingyon.com' : 'http://localhost:8000'
+export const LOGIN_WS_URL = process.env.NODE_ENV == 'production' ? 'wss://api.receiver.qingyon.com/login' : 'ws://localhost:8000/login'
