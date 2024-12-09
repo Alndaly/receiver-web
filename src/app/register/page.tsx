1803be071bc7e8bbe2d8f6b3be843619560e543c
Kinda Hall <1142704468@qq.com>
2024-12-10 00:01:15 +0800

    feat: 🎸 配置环境以及优化注册页面

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,8 +5,7 @@ export default function RegisterPage() {
 		<div
 			className='flex h-screen w-full items-center justify-center px-4'
 			style={{
-				backgroundImage:
-					'linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)',
+				backgroundImage: 'linear-gradient(to top, #fddb92 0%, #d1fdff 100%)',
 			}}>
 			<RegisterForm />
 		</div>
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,4 @@
-export const API_PREFIX = 'http://localhost:8000'
 export const UNIAPI_PREFIX = 'https://api.uniapi.top';
-export const LOGIN_WS_URL = 'ws://localhost:8000/login'
+console.log(process.env.NODE_ENV, process.env.TZ)
+export const API_PREFIX = process.env.NODE_ENV == 'production' ? 'https://api.receiver.qingyun.com' : 'http://localhost:8000'
+export const LOGIN_WS_URL = process.env.NODE_ENV == 'production' ? 'wss://api.receiver.qingyun.com/login' : 'ws://localhost:8000/login'
