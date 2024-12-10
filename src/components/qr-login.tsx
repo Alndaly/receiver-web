8bf7e0c82b7cec6ba1ff98dc6c48fc7d0dca2202
Kinda Hall <1142704468@qq.com>
2024-12-10 11:10:41 +0800

    feat: 🎸 优化登录页面样式

--- src/components/qr-login.tsx
+++ src/components/qr-login.tsx
@@ -75,8 +75,7 @@ const QrLogin = () => {
 		<div className='flex justify-center items-center p-5 flex-col'>
 			{!qrCode && (
 				<div
-					className='aspect-square rounded flex justify-center items-center'
-					style={{ width: '300px' }}>
+					className='aspect-square rounded flex justify-center items-center w-[250px]'>
 					二维码加载中
 					<span className='ml-3 relative flex h-3 w-3'>
 						<span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75'></span>
