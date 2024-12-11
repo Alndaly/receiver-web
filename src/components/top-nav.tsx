f39c56eb6ed1a65b7025bf70da46f8dd1897096b
Kinda Hall <1142704468@qq.com>
2024-12-11 11:59:18 +0800

    fix: 🐛 修复top-nav打包

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -9,7 +9,7 @@ import {
 import routers, { findRouteByPath } from '@/config/router';
 import Link from 'next/link';
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 interface Crumb {
@@ -19,22 +19,23 @@ interface Crumb {
 
 const TopNav = () => {
 	const pathname = usePathname();
-	const searchParams = useSearchParams();
 	const [crumbs, setCrumbs] = useState<Crumb[]>([]);
 
 	useEffect(() => {
-		console.log(pathname)
 		const paths = pathname
 			.split('/')
 			.filter((path) => path !== '')
 			.map((path, index, array) => {
 				return {
-					title: findRouteByPath(routers, `/${array.slice(0, index + 1).join('/')}`)?.title,
+					title: findRouteByPath(
+						routers,
+						`/${array.slice(0, index + 1).join('/')}`
+					)?.title,
 					path: `/${array.slice(0, index + 1).join('/')}`,
 				};
 			});
 		setCrumbs(paths);
-	}, [pathname, searchParams]);
+	}, [pathname]);
 
 	return (
 		<Breadcrumb>
