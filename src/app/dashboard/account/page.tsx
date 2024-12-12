ae677247f0cfc092ebc6367443a102c5d0e24006
Kinda Hall <1142704468@qq.com>
2024-12-12 16:50:50 +0800

    feat: 🎸 优化部分图片loading

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { getMyInfo } from '@/service/user';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
@@ -33,13 +34,10 @@ const AccountPage = () => {
 				<div className='flex flex-col gap-5'>
 					<div className='flex flex-row gap-5 items-center'>
 						<div className='font-bold'>头像</div>
-						<Image
-							src={userInfo.avatar}
-							alt='avatar'
-							width={100}
-							height={100}
-							className='rounded'
-						/>
+						<Avatar className='w-24 h-24'>
+							<AvatarImage src={userInfo.avatar} alt={userInfo.nickname} />
+							<AvatarFallback>Avatar</AvatarFallback>
+						</Avatar>
 					</div>
 					<div className='flex flex-row gap-5 items-center'>
 						<div className='font-bold'>昵称</div>
--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -62,7 +62,7 @@ export function NavUser({}) {
 							className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
 							<Avatar className='h-8 w-8 rounded-lg'>
 								<AvatarImage src={user.avatar} alt={user.nickname} />
-								<AvatarFallback className='rounded-lg'>NN</AvatarFallback>
+								<AvatarFallback className='rounded-lg'>Avatar</AvatarFallback>
 							</Avatar>
 							<div className='grid flex-1 text-left text-sm leading-tight'>
 								<span className='truncate font-semibold'>{user.nickname}</span>
