eb381c9af15af7658256238b2ee97216a7a87ccc
Kinda Hall <1142704468@qq.com>
2024-12-17 17:09:55 +0800

    feat: 🎸 完善任务详情页

--- src/app/dashboard/task/detail/loading.tsx
+++ src/app/dashboard/task/detail/loading.tsx
@@ -1,5 +1,5 @@
 const LoadingPage = () => {
-	return <div>loading</div>;
+	return <div>任务加载中...</div>;
 };
 
 export default LoadingPage;
--- src/app/dashboard/task/detail/page.tsx
+++ src/app/dashboard/task/detail/page.tsx
@@ -2,23 +2,64 @@
 
 import { getTaskDetail } from '@/service/task';
 import { useEffect, useState } from 'react';
-import { useParams, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
+import { z } from 'zod';
+import { CalendarIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { Card } from '@/components/ui/card';
+import { Textarea } from '@/components/ui/textarea';
+
+const taskFormSchema = z.object({
+	title: z.string().min(5).max(40),
+	description: z.string().min(10).max(200),
+	priority: z.number().min(0).max(5),
+	status: z.string().min(0).max(50),
+});
 
 interface TaskDetail {
 	title: string;
 	description: string;
 	create_time: string;
 	update_time: string;
+	priority: string;
 	status: string;
 }
 
 const TaskDetailPage = () => {
 	const searchParams = useSearchParams();
 	const [task, setTask] = useState<TaskDetail | null>(null);
-	useEffect(() => {
-		onGetTask();
-	}, []);
+
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof taskFormSchema>>({
+		resolver: zodResolver(taskFormSchema),
+		defaultValues: {
+			title: '',
+			description: '',
+			priority: 1,
+			status: '',
+		},
+	});
 
 	const onGetTask = async () => {
 		const id = searchParams.get('id');
@@ -32,15 +73,136 @@ const TaskDetailPage = () => {
 			return;
 		}
 		setTask(res);
+		form.reset({
+			title: res.title,
+			description: res.description,
+			priority: res.priority,
+			status: res.status,
+		});
 	};
 
+	const onSuccess = async (values: z.infer<typeof taskFormSchema>) => {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	};
+
+	const onError = (errors: any) => {
+		// Handle validation errors here.
+		console.log(errors);
+	};
+
+	useEffect(() => {
+		onGetTask();
+	}, []);
+
 	return (
-		<div>
+		<div className='pb-5 flex-1 h-1 overflow-auto'>
 			{task ? (
-				<>
-					<p>{task.title}</p>
-					<p>{task.description}</p>
-				</>
+				<Card className='p-5'>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSuccess, onError)}
+							className='space-y-5'>
+							<FormField
+								control={form.control}
+								name='title'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className='flex flex-col gap-2'>
+											标题
+											<FormDescription>
+												起个爽快明了的名字更有利于行动起来哦
+											</FormDescription>
+										</FormLabel>
+										<div className='flex flex-col gap-2'>
+											<FormControl>
+												<Input placeholder='标题' {...field} />
+											</FormControl>
+											<FormMessage />
+										</div>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='description'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className='flex flex-col gap-2'>
+											详情
+											<FormDescription>
+												任务的详情也是很重要的，这里可以记载你真正的任务步骤。
+											</FormDescription>
+										</FormLabel>
+										<div className='flex flex-col gap-2'>
+											<FormControl>
+												<Textarea placeholder='详情' {...field} />
+											</FormControl>
+											<FormMessage />
+										</div>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='priority'
+								render={({ field }) => (
+									<FormItem className='flex justify-between items-center'>
+										<FormLabel className='flex flex-col gap-2'>
+											优先级
+											<FormDescription>
+												范围1-5，不得在这个范围之外。
+											</FormDescription>
+										</FormLabel>
+										<div className='flex flex-col gap-2'>
+											<FormControl>
+												<Input placeholder='优先级' {...field} />
+											</FormControl>
+											<FormMessage />
+										</div>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='status'
+								render={({ field }) => (
+									<FormItem className='flex justify-between items-center'>
+										<FormLabel className='flex flex-col gap-2'>
+											状态
+											<FormDescription>
+												当前共有五种类型，分别具有不同含义，可以自行修改。
+											</FormDescription>
+										</FormLabel>
+										<div className='flex flex-col gap-2'>
+											<Select
+												onValueChange={field.onChange}
+												defaultValue={field.value}>
+												<FormControl>
+													<SelectTrigger>
+														<SelectValue placeholder='Select a verified email to display' />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent>
+													<SelectItem value='todo'>待做</SelectItem>
+													<SelectItem value='doing'>进行中</SelectItem>
+													<SelectItem value='done'>完成</SelectItem>
+													<SelectItem value='canceled'>取消</SelectItem>
+													<SelectItem value='failed'>失败</SelectItem>
+												</SelectContent>
+											</Select>
+											<FormMessage />
+										</div>
+									</FormItem>
+								)}
+							/>
+							<Button className='w-full' variant={'outline'} type='submit'>
+								保存
+							</Button>
+						</form>
+					</Form>
+				</Card>
 			) : (
 				<>无对应数据</>
 			)}
