d1a3e5c6c472469aa98ed9084bc15faf2f7ecd8b
Kinda Hall <1142704468@qq.com>
2025-01-13 23:41:33 +0800

    feat: 🎸 恢复文档页

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -290,7 +290,7 @@ export function LoginForm() {
 							</div>
 							<Link
 								className='underline text-center text-sm'
-								href={'#'}
+								href={'https://docs.receiver.qingyon.com'}
 								target='_blank'>
 								docs here
 							</Link>
